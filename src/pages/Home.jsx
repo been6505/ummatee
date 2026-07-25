7a0a0f28e6175a85dee053d875e4287179999acd
Ummatee <akasitlove@gmail.com>
2026-07-25 19:00:56 +0700

    Add top-light to bottom-dark gradient background on the public home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -264,7 +264,7 @@ export default function Home() {
   }
 
   return (
-    <main className="page">
+    <main className="page home-page">
       {showAnnouncement && (
         <div className="site-announcement">
           <span className="site-announcement-text">
